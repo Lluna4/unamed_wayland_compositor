44f92a030cd4624fabce4463f8a8a2edf762042a
Luna <holacarly500@gmail.com>
2024-12-11 05:06:14 +0100

    Added functions for wl_surface.attach and wl_surface.damage

--- src/include/compositor.cpp
+++ src/include/compositor.cpp
@@ -8,12 +8,21 @@ void destroy_surface(struct wl_resource *resource)
     free(resource->data);
 }
 
-void destroy_region(struct wl_resource *resource)
+void attach_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *buff, int32_t x, int32_t y)
 {
-    regions.erase(*(uint32_t *)(resource->data));
-    free(resource->data);
+    surfaces.find(*(uint32_t *)(resource->data))->second.pending_buff = buffer(buff, x, y);
 }
-
+void damage_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height)
+{
+    auto surface = surfaces.find(*(uint32_t *)(resource->data));
+    if (surface != surfaces.end())
+    {
+        surface->second.damage_height = height;
+        surface->second.damage_width = width;
+        surface->second.damage_x = x;
+        surface->second.damage_y = y;
+    }
+}
 void create_surface(struct wl_client *client, struct wl_resource *resource, uint32_t id)
 {
     wl_resource *ret = wl_resource_create(client, &wl_surface_interface, wl_resource_get_version(resource), id);
@@ -25,7 +34,15 @@ void create_surface(struct wl_client *client, struct wl_resource *resource, uint
     uint32_t *id_ptr = (uint32_t *)malloc(1 * sizeof(uint32_t));
     surfaces.emplace(id, surface(id, 0, 0));
     *id_ptr = id;
-    wl_resource_set_implementation(ret, &wl_surface_interface, id_ptr, destroy_surface);
+    //wl_resource_set_implementation(ret, &wl_surface_interface, id_ptr, destroy_surface); TODO set actual implementation
+}
+
+
+
+void destroy_region(struct wl_resource *resource)
+{
+    regions.erase(*(uint32_t *)(resource->data));
+    free(resource->data);
 }
 
 void create_region(struct wl_client *client,struct wl_resource *resource,uint32_t id)
@@ -33,13 +50,13 @@ void create_region(struct wl_client *client,struct wl_resource *resource,uint32_
     wl_resource *ret = wl_resource_create(client, &wl_region_interface, wl_resource_get_version(resource), id);
     if (!ret)
     {
-        printf("surface creation failed!\n");
+        printf("region creation failed!\n");
         return;
     }
     uint32_t *id_ptr = (uint32_t *)malloc(1 * sizeof(uint32_t));
     regions.emplace(id, region(id, 0, 0));
     *id_ptr = id;
-    wl_resource_set_implementation(ret, &wl_surface_interface, id_ptr, destroy_region);
+    //wl_resource_set_implementation(ret, &wl_region_interface, id_ptr, destroy_region); TODO set actual implementation
 }
 
 void bind_compositor(struct wl_client *client,void *data,uint32_t version,uint32_t id) 
--- src/include/compositor.h
+++ src/include/compositor.h
@@ -1,12 +1,66 @@
 #pragma once
 #include <wayland-server.h>
 #include <stdio.h>
-#include "surface.h"
-#include "region.h"
+#include <map>
+
+struct buffer
+{
+    buffer()
+    {
+        content = nullptr;
+        x = 0;
+        y = 0;
+        damage_x = 0;
+        damage_y = 0; 
+        damage_width = 0;
+        damage_height = 0;
+    }
+    buffer(wl_resource *content_, int x_, int y_)
+    :content(content_), x(x_), y(y_)
+    {}
+    wl_resource *content;
+    int x;
+    int y;
+    int damage_x;
+    int damage_y;
+    int damage_width;
+    int damage_height;
+};
+
+struct surface
+{
+    surface(uint32_t id_, int x_, int y_)
+    :id(id_), x(x_), y(y_)
+    {
+        damage_x = 0;
+        damage_y = 0; 
+        damage_width = 0;
+        damage_height = 0;
+    }
+    uint32_t id;
+    int x;
+    int y;
+    int damage_x;
+    int damage_y;
+    int damage_width;
+    int damage_height;
+    buffer pending_buff;
+};
+
+struct region
+{
+    region(uint32_t id_, int x_, int y_)
+    :id(id_), x(x_), y(y_)
+    {}
+    uint32_t id;
+    int x;
+    int y;
+};
 
 void destroy_surface(struct wl_resource *resource);
-void destroy_region(struct wl_resource *resource);
+void attach_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *buff, int32_t x, int32_t y);
 void create_surface(struct wl_client *client, struct wl_resource *resource, uint32_t id);
+void destroy_region(struct wl_resource *resource);
 void create_region(struct wl_client *client,struct wl_resource *resource,uint32_t id);
 
 static const struct wl_compositor_interface wl_compositor_implementation =

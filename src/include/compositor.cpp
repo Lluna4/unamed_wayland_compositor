bdaef87cbdf24b3902a9eccdb2398f7b3acd4552
Luna <holacarly500@gmail.com>
2024-12-12 06:01:22 +0100

    Implemented almost all wl_surface

--- src/include/compositor.cpp
+++ src/include/compositor.cpp
@@ -1,20 +1,19 @@
 #include "compositor.h"
 
-std::map<uint32_t, region> regions;
-std::map<uint32_t, surface> surfaces;
-void destroy_surface(struct wl_resource *resource)
+std::map<wl_client *, region> regions;
+std::map<wl_client *, surface> surfaces;
+void destroy_surface(struct wl_client *client, struct wl_resource *resource)
 {
-    surfaces.erase(*(uint32_t *)(resource->data));
-    free(resource->data);
+    surfaces.erase(client);
 }
 
 void attach_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *buff, int32_t x, int32_t y)
 {
-    surfaces.find(*(uint32_t *)(resource->data))->second.pending_buff = buffer(buff, x, y);
+    surfaces.find(client)->second.pending_buff = buffer(buff, x, y);
 }
 void damage_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height)
 {
-    auto surface = surfaces.find(*(uint32_t *)(resource->data));
+    auto surface = surfaces.find(client);
     if (surface != surfaces.end())
     {
         surface->second.damage_height = height;
@@ -23,6 +22,71 @@ void damage_surface(struct wl_client *client, struct wl_resource *resource, int3
         surface->second.damage_y = y;
     }
 }
+
+void damage_buffer_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height)
+{
+    auto surface = surfaces.find(client);
+    if (surface != surfaces.end())
+    {
+        surface->second.pending_buff.damage_height = height;
+        surface->second.pending_buff.damage_width = width;
+        surface->second.pending_buff.damage_x = x;
+        surface->second.pending_buff.damage_y = y;
+    }
+}
+
+void frame_surface(struct wl_client *client, struct wl_resource *resource, uint32_t callback)
+{
+    printf("wl_surface.frame() is not implmented\n");
+
+}
+
+void set_opaque_region_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region)
+{
+    printf("wl_surface.set_opaque_region() is not implmented\n");
+}
+
+void set_input_region_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region)
+{
+    printf("wl_surface.set_input_region() is not implmented\n");
+}
+
+void commit_surface(struct wl_client *client, struct wl_resource *resource)
+{
+    auto surface = surfaces.find(client);
+    if (surface != surfaces.end())
+    {
+        surface->second.pending_buff = buffer(nullptr, 0, 0);
+        //send signal to backend to print to buffer and switch buffers
+    }
+}
+
+void set_buffer_transform_surface(struct wl_client *client, struct wl_resource *resource, int32_t transform)
+{
+    printf("wl_surface.set_buffer_transform() is not implmented %i\n", transform);
+}
+
+void set_buffer_scale_surface(struct wl_client *client, struct wl_resource *resource, int32_t scale)
+{
+    auto surface = surfaces.find(client);
+    if (surface != surfaces.end())
+    {
+        surface->second.pending_buff.buffer_scale = scale;
+        printf("New buffer scale is %i\n", scale);
+    }
+}
+
+void offset_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y)
+{
+    auto surface = surfaces.find(client);
+    if (surface != surfaces.end()) 
+    {
+        surface->second.x = x;
+        surface->second.y = y;
+        printf("New offset x: %i, y: %i\n", x, y);
+    }
+}
+
 void create_surface(struct wl_client *client, struct wl_resource *resource, uint32_t id)
 {
     wl_resource *ret = wl_resource_create(client, &wl_surface_interface, wl_resource_get_version(resource), id);
@@ -31,18 +95,14 @@ void create_surface(struct wl_client *client, struct wl_resource *resource, uint
         printf("surface creation failed!\n");
         return;
     }
-    uint32_t *id_ptr = (uint32_t *)malloc(1 * sizeof(uint32_t));
-    surfaces.emplace(id, surface(id, 0, 0));
-    *id_ptr = id;
-    //wl_resource_set_implementation(ret, &wl_surface_interface, id_ptr, destroy_surface); TODO set actual implementation
+    printf("created surface with id %i\n", id);
+    surfaces.emplace(client, surface(id, 0, 0));
+    wl_resource_set_implementation(ret, &wl_surface_implementation, nullptr, NULL);
 }
 
-
-
-void destroy_region(struct wl_resource *resource)
+void destroy_region(struct wl_client *client, struct wl_resource *resource)
 {
-    regions.erase(*(uint32_t *)(resource->data));
-    free(resource->data);
+    regions.erase(client);
 }
 
 void create_region(struct wl_client *client,struct wl_resource *resource,uint32_t id)
@@ -53,19 +113,18 @@ void create_region(struct wl_client *client,struct wl_resource *resource,uint32_
         printf("region creation failed!\n");
         return;
     }
-    uint32_t *id_ptr = (uint32_t *)malloc(1 * sizeof(uint32_t));
-    regions.emplace(id, region(id, 0, 0));
-    *id_ptr = id;
+    regions.emplace(client, region(id, 0, 0));
     //wl_resource_set_implementation(ret, &wl_region_interface, id_ptr, destroy_region); TODO set actual implementation
 }
 
 void bind_compositor(struct wl_client *client,void *data,uint32_t version,uint32_t id) 
 {
-    struct wl_resource *resource = wl_resource_create(client, &wl_compositor_interface,version,id);
+    struct wl_resource *resource = wl_resource_create(client, &wl_compositor_interface, version,id);
     if (!resource) 
     {
         wl_client_post_no_memory(client);
         return;
     }
-    wl_resource_set_implementation(resource, &wl_compositor_implementation,data, NULL);
+    wl_resource_set_implementation(resource, &wl_compositor_implementation, nullptr, NULL);
+    printf("compositor binded with id %i\n", id);
 }
--- src/include/compositor.h
+++ src/include/compositor.h
@@ -25,6 +25,7 @@ struct buffer
     int damage_y;
     int damage_width;
     int damage_height;
+    int buffer_scale;
 };
 
 struct surface
@@ -57,11 +58,20 @@ struct region
     int y;
 };
 
-void destroy_surface(struct wl_resource *resource);
+void destroy_surface(struct wl_client *client, struct wl_resource *resource);
 void attach_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *buff, int32_t x, int32_t y);
+void damage_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height);
+void damage_buffer_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height);
+void frame_surface(struct wl_client *client, struct wl_resource *resource, uint32_t callback); //NOT implmented
+void set_opaque_region_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region); //NOT implemented
+void set_input_region_surface(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region); //NOT implemented
+void commit_surface(struct wl_client *client, struct wl_resource *resource);
+void set_buffer_transform_surface(struct wl_client *client, struct wl_resource *resource, int32_t transform);//NOT implemented
+void set_buffer_scale_surface(struct wl_client *client, struct wl_resource *resource, int32_t scale);
+void offset_surface(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y);
 void create_surface(struct wl_client *client, struct wl_resource *resource, uint32_t id);
-void destroy_region(struct wl_resource *resource);
-void create_region(struct wl_client *client,struct wl_resource *resource,uint32_t id);
+void destroy_region(struct wl_client *client, struct wl_resource *resource);
+void create_region(struct wl_client *client, struct wl_resource *resource,uint32_t id);
 
 static const struct wl_compositor_interface wl_compositor_implementation =
 {
@@ -70,3 +80,18 @@ static const struct wl_compositor_interface wl_compositor_implementation =
 };
 
 void bind_compositor(struct wl_client *client,void *data,uint32_t version,uint32_t id);
+
+static const struct wl_surface_interface wl_surface_implementation = 
+{
+    .destroy = destroy_surface,
+    .attach = attach_surface,
+    .damage = damage_surface,
+    .frame = frame_surface,
+    .set_opaque_region = set_opaque_region_surface,
+    .set_input_region = set_input_region_surface,
+    .commit = commit_surface,
+    .set_buffer_transform = set_buffer_transform_surface,
+    .set_buffer_scale = set_buffer_scale_surface,
+    .damage_buffer = damage_buffer_surface,
+    .offset = offset_surface
+};
